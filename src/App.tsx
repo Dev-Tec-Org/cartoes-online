36bf69c50f0b53230d030a402290c83ea7454723
Guilherme <guilherme@zscansoftware.com>
2023-08-28 22:06:17 -0300

    Responsividade desktop

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import AppRoutes from './Routes';
 const App: React.FC = () => {
   return (
     <Router>
-      <Box w={"full"} h={"100%"} backgroundColor={"blackAlpha.800"} >
+      <Box w={"full"} h={"100vh"} backgroundColor={"blackAlpha.800"} >
         <Box w={"max-content"} h={"max-content"} mx={"auto"} py={"3%"}>
           <AppRoutes />
         </Box>

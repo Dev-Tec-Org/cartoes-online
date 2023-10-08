75e3a5b7e75e4f1a8a6eb5aec9ad8ff72acc25d1
Guilherme <guilherme@zscansoftware.com>
2023-10-08 20:11:14 -0300

    adicionando Botão ded compartilhar.

--- src/cartoes/samantha-lowes/index.tsx
+++ src/cartoes/samantha-lowes/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import { Box, Center, Image, Text, Divider, Flex, Link } from "@chakra-ui/react";
 import Contacts from './components/Contacts';
-import imagemCapa from '../../assets/samantha-lowes/imgs/ImagemCapa.jpg'
+import imagemCapa from '../../assets/samantha-lowes/imgs/ImagemCapa.jpg';
+import comp from "../../assets/samantha-lowes/icons/Share.png";
+
 export const SamanthaLowes = () => {
+    const handleShare = async () => {
+        try {
+            await navigator.share({
+                title: 'Samantha Lowes',
+                text: 'Tapa os ouvidos e vai!',
+                url: 'https://onlinecartoes.com/samantha-lowes',
+            });
+            console.log('Conteúdo compartilhado com sucesso!');
+        } catch (error) {
+            console.error('Erro ao compartilhar:', error);
+        }
+    };
+
     return (
         <Flex
             justifyContent="center"
@@ -11,7 +26,12 @@ export const SamanthaLowes = () => {
             w={"100%"}
             backgroundColor={"black"}
         >
-            <Box maxW={"428px"} h={'full'} backgroundColor={"#000000"}>
+            <Box maxW={"428px"} h={'full'} backgroundColor={"#000000"} position={'relative'}>
+
+                <Box zIndex={4} position={"absolute"} marginLeft={"89%"} top={'10px'} cursor={'pointer'} onClick={() => { handleShare() }}>
+                    <Image src={comp} alt='Compartilhar' w={"30px"} h={"30px"} />
+                </Box>
+
                 <Image src={imagemCapa} />
                 <Box backgroundColor={"#000000"}>
                     <Contacts />
